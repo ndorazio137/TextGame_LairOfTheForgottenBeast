32ca1c658bb4b90eaa38627d7352f34ba582d662
ndorazio137 <ndorazio@csumb.edu>
2021-06-19 22:39:26 -0700

    Setting up initial project

---
+++ src/main/java/LairOfTheForgottenBeast/LairOfTheForgottenBeastApplication.java
@@ -0,0 +1,13 @@
+package LairOfTheForgottenBeast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LairOfTheForgottenBeastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LairOfTheForgottenBeastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LairOfTheForgottenBeast/LairOfTheForgottenBeastApplicationTests.java
@@ -0,0 +1,13 @@
+package LairOfTheForgottenBeast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LairOfTheForgottenBeastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
