0c501e4312ec36f85acfe6e63ea371f507bbb47a
ndorazio137 <ndorazio@csumb.edu>
2021-06-21 01:22:33 -0700

    save state

--- src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
+++ src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
@@ -1,14 +1,13 @@
 package LairOfTheForgottenBeast.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @Controller 
 public class HomeController {
   
-   @RequestMapping("/") 
+   @GetMapping("/") 
    public @ResponseBody String greeting() { 
       return "Hello, World"; 
    }
