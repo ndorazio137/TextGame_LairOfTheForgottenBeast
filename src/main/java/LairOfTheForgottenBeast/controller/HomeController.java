301be83640627317f58712bdd9a9ad0f4209d8b5
ndorazio137 <ndorazio@csumb.edu>
2021-06-22 21:41:16 -0700

    Created Basic v1 Room Object

--- src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
+++ src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
@@ -1,6 +1,7 @@
 package LairOfTheForgottenBeast.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
   
    @GetMapping("/") 
-   public @ResponseBody String greeting() { 
-      return "Hello, World"; 
+   public String greeting( Model model ) { 
+      return "console"; 
    }
   
+   
 }
