cc61e0aec364b896e2526db2736b122e94973c25
kyleoakes <koakes@csumb.edu>
2021-06-23 01:47:36 -0700

    save state

--- src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
+++ src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import LairOfTheForgottenBeast.domain.map.Room;
@@ -15,9 +16,16 @@ import LairOfTheForgottenBeast.domain.map.RoomRepository;
 public class HomeController {
    @Autowired
    RoomRepository roomRepository; 
+   
    @GetMapping("/") 
    public String greeting( Model model ) { 
       return "console"; 
+   }
+   
+   @PostMapping("/console")
+   public String sendCommand( Model model ) {
+      System.out.println("POST Request received: /console");
+      return "console";
    }
    
    @GetMapping("/rooms") 
@@ -25,6 +33,6 @@ public class HomeController {
       List<Room> rooms = roomRepository.findAllOrderByIdDesc();
       model.addAttribute("rooms", rooms);
       return "room_list"; 
-   }  
+   }
    
 }
