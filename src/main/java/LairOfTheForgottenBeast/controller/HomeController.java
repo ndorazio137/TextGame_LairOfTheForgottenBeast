012a92978303f62c314db4749acd12cea3f7bf02
kyleoakes <koakes@csumb.edu>
2021-06-23 17:28:09 -0700

    created CommandInfo object to be passed from client to server. currently only contains a String commandString, but this will let us pass more fields later if we need to. Note: server crashes when trying to execute a "go [direction]" command because the player is null

--- src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
+++ src/main/java/LairOfTheForgottenBeast/controller/HomeController.java
@@ -2,12 +2,17 @@ package LairOfTheForgottenBeast.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import LairOfTheForgottenBeast.domain.CommandInfo;
 import LairOfTheForgottenBeast.domain.map.Room;
 import LairOfTheForgottenBeast.domain.map.RoomRepository;
 import LairOfTheForgottenBeast.service.GameService;
@@ -26,11 +31,11 @@ public class HomeController {
    }
    
    @PostMapping("/console")
-   public String sendCommand( Model model ) {
+   public String sendCommand(@Valid CommandInfo commandInfo, BindingResult result, Model model ) {
       System.out.println("POST Request received: /console");
       // TODO: update this line to reflect  
       // the correct form field name
-//      gameService.processCommand(model.commandString);
+      gameService.processCommand(commandInfo.getCommandString());
       return "console";
    }
    
