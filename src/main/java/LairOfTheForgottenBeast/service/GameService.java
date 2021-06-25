f2dda53b50f4770cd9791f534fe7978a5b6df1bb
kyleoakes <koakes@csumb.edu>
2021-06-25 04:29:54 -0700

    commented out the new code...because apparently roomRepository is null...

--- src/main/java/LairOfTheForgottenBeast/service/GameService.java
+++ src/main/java/LairOfTheForgottenBeast/service/GameService.java
@@ -14,12 +14,14 @@ import LairOfTheForgottenBeast.domain.map.WorldMap;
 
 public class GameService
 {
+   @Autowired
+   RoomRepository roomRepository;
    private CommandTokenizer commandTokenizer = new CommandTokenizer();
    private CommandInterpreter commandInterpreter = new CommandInterpreter();
-   WorldMap worldMap = this.generateWorldMap(3,3,1); // TODO: generate the world map. This should be the last thing we need to get the player exploring.
+   WorldMap worldMap = this.generateWorldMap(3,3,1); 
    Player player = this.generatePlayer();
    GameState gameState = new GameState(worldMap,player);
-   RoomRepository roomRepository;
+   
    
    public String processCommand(String publicCommand)
    {
@@ -33,27 +35,28 @@ public class GameService
    public WorldMap generateWorldMap(int sizeX, int sizeY, int sizeZ) {
       Room[][][] rooms = new Room[sizeX][sizeY][sizeZ];
       
-//      rooms[0][0][0] = new Room(1, "Sinkhole", "The air is moist and earthy.");
-//      rooms[0][1][0] = new Room(2, "Web Room", "There are webs covering every surface, and you hear a distant sound of skittering.");
-//      rooms[0][2][0] = new Room(3, "Open Chamber", "This is an open chamber with nothing exciting to see here.");
-//      rooms[1][0][0] = new Room(4, "Armory", "This room is the armory. There are weapons and armor neatly arranged for accessibility in case of an emergency.");
-//      rooms[1][1][0] = new Room(5, "Dining Hall", "This is the dining hall.");
-//      rooms[1][2][0] = new Room(6, "Library", "You are in a library.");
-//      rooms[2][0][0] = new Room(7, "Living Area", "This is a large living area with furniture.");
-//      rooms[2][1][0] = new Room(8, "Barracks", "This is a dorm room that the guards use as barracks.");
-//      rooms[2][2][0] = new Room(9, "Kitchen", "The kitchen is a room with an iron pot over a cooking fire.");
+      rooms[0][0][0] = new Room(1, "Sinkhole", "The air is moist and earthy.");
+      rooms[0][1][0] = new Room(2, "Web Room", "There are webs covering every surface, and you hear a distant sound of skittering.");
+      rooms[0][2][0] = new Room(3, "Open Chamber", "This is an open chamber with nothing exciting to see here.");
+      rooms[1][0][0] = new Room(4, "Armory", "This room is the armory. There are weapons and armor neatly arranged for accessibility in case of an emergency.");
+      rooms[1][1][0] = new Room(5, "Dining Hall", "This is the dining hall.");
+      rooms[1][2][0] = new Room(6, "Library", "You are in a library.");
+      rooms[2][0][0] = new Room(7, "Living Area", "This is a large living area with furniture.");
+      rooms[2][1][0] = new Room(8, "Barracks", "This is a dorm room that the guards use as barracks.");
+      rooms[2][2][0] = new Room(9, "Kitchen", "The kitchen is a room with an iron pot over a cooking fire.");
       
-      List<Room> roomList = roomRepository.findAllOrderByIdDesc();
-      int roomListIndex = 0;
-      for (int z = 0; z < sizeZ; z++) {
-         for (int y = 0; y < sizeY; y++) {
-            for (int x = 0; x < sizeX; x++) {
-               rooms[x][y][z] = roomList.get(roomListIndex++);
-            }
-         }
-      }
-      
-      WorldMap worldMap = new WorldMap(rooms, sizeX, sizeY, sizeZ);
+//      List<Room> roomList = roomRepository.findAllOrderByIdDesc();
+//      int roomListIndex = 0;
+//      for (int z = 0; z < sizeZ; z++) {
+//         for (int y = 0; y < sizeY; y++) {
+//            for (int x = 0; x < sizeX; x++) {
+//               rooms[x][y][z] = roomList.get(roomListIndex++);
+//            }
+//         }
+//      }
+//      
+//      WorldMap worldMap = new WorldMap(rooms, sizeX, sizeY, sizeZ);
+      WorldMap worldMap = new WorldMap(rooms,3,3,1);
       
       return worldMap;
    }
