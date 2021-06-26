07e7356231ab06e65b0ebd507b6a5ab6927c6280
btjam <btjam@10.0.0.146>
2021-06-25 20:15:09 -0700

    Added traps and puzzles that implement Prop.

--- src/main/java/LairOfTheForgottenBeast/factory/PropFactory.java
+++ src/main/java/LairOfTheForgottenBeast/factory/PropFactory.java
@@ -4,6 +4,8 @@ import LairOfTheForgottenBeast.domain.NatureProp;
 import LairOfTheForgottenBeast.domain.Decoration;
 import LairOfTheForgottenBeast.domain.Item;
 import LairOfTheForgottenBeast.domain.Prop;
+import LairOfTheForgottenBeast.domain.Puzzle;
+import LairOfTheForgottenBeast.domain.Trap;
 
 public class PropFactory implements AbstractFactory<Prop>
 {
@@ -19,9 +21,17 @@ public class PropFactory implements AbstractFactory<Prop>
       {
          return new Decoration(); // human-created decorations like room styles
       }
-      else if ("natureProp".equalsIgnoreCase(propType)) 
+      else if ("NatureProp".equalsIgnoreCase(propType)) 
       {
          return new NatureProp(); // Boulders, trees, water, all natural
+      }
+      else if ("Trap".equalsIgnoreCase(propType)) 
+      {
+         return new Trap(); // pits, trip wires, loose tiles, etc. 
+      }
+      else if ("Puzzle".equalsIgnoreCase(propType)) 
+      {
+         return new Puzzle(); // puzzles 
       }
       return null;
    }
