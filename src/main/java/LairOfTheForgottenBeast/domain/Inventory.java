fb2ae744e4502a608656bf0b2f642d5fcc28560a
ndorazio137 <ndorazio@csumb.edu>
2021-06-27 01:21:17 -0700

    Added javadoc to Inventory interface

--- src/main/java/LairOfTheForgottenBeast/domain/Inventory.java
+++ src/main/java/LairOfTheForgottenBeast/domain/Inventory.java
@@ -1,7 +1,16 @@
 package LairOfTheForgottenBeast.domain;
 
+/**
+ * A top level interface for the Inventory Management System.
+ * 
+ * @author Nick D'Orazio
+ * @version 0.0.1
+ * @dateCreated 6/26/2021
+ * @lastUpdated 6/26/2021 
+ * @updateBy unchanged since creation
+ */
 public interface Inventory {
-   public boolean addItem(Item inventoryObject);
+   public boolean addItem(Item item);
    public Item removeItem(Item item);
    public int getSize();
    public boolean clear();
