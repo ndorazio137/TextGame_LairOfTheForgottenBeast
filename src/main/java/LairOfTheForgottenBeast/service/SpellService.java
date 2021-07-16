daa7d81946fd2661992fc8c0d7dd35c624fd4910
Krowi <Krowi@10.0.0.113>
2021-07-15 20:41:07 -0700

    added a castSpell method

--- src/main/java/LairOfTheForgottenBeast/service/SpellService.java
+++ src/main/java/LairOfTheForgottenBeast/service/SpellService.java
@@ -1,5 +1,11 @@
 package LairOfTheForgottenBeast.service;
 
+import java.util.List;
+
 public class SpellService {
 	
+	public String castSpell(List<String> spell) {
+		
+		return "You cast a spell: " + spell;
+	}
 }
