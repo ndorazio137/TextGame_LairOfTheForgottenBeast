528d1d306a0c8c8c43b271eef4432eae47cf7e23
Krowi <Krowi@10.0.0.113>
2021-07-15 22:41:14 -0700

    removed unused prop subclasses

--- src/main/java/LairOfTheForgottenBeast/factory/PropFactory.java
+++ src/main/java/LairOfTheForgottenBeast/factory/PropFactory.java
@@ -2,10 +2,7 @@ package LairOfTheForgottenBeast.factory;
 
 import LairOfTheForgottenBeast.domain.prop.Decoration;
 import LairOfTheForgottenBeast.domain.prop.Item;
-import LairOfTheForgottenBeast.domain.prop.NatureProp;
 import LairOfTheForgottenBeast.domain.prop.Prop;
-import LairOfTheForgottenBeast.domain.prop.Puzzle;
-import LairOfTheForgottenBeast.domain.prop.Trap;
 
 public class PropFactory implements AbstractPropFactory<Prop>
 {
@@ -25,18 +22,6 @@ public class PropFactory implements AbstractPropFactory<Prop>
       {
          return new Decoration(); // human-created decorations like room styles
       }
-      if ("NatureProp".equalsIgnoreCase(propType)) 
-      {
-         return new NatureProp(); // Boulders, trees, water, all natural
-      }
-      if ("Trap".equalsIgnoreCase(propType)) 
-      {
-         return new Trap(); // pits, trip wires, loose tiles, etc. 
-      }
-      if ("Puzzle".equalsIgnoreCase(propType)) 
-      {
-         return new Puzzle(); // puzzles 
-      }
       return null;
    }
 
@@ -49,18 +34,6 @@ public class PropFactory implements AbstractPropFactory<Prop>
       else if ("Decoration".equalsIgnoreCase(propType)) 
       {
          return new Decoration(); // human-created decorations like room styles
-      }
-      else if ("NatureProp".equalsIgnoreCase(propType)) 
-      {
-         return new NatureProp(); // Boulders, trees, water, all natural
-      }
-      else if ("Trap".equalsIgnoreCase(propType)) 
-      {
-         return new Trap(); // pits, trip wires, loose tiles, etc. 
-      }
-      else if ("Puzzle".equalsIgnoreCase(propType)) 
-      {
-         return new Puzzle(); // puzzles 
       }
       return null;
    }
