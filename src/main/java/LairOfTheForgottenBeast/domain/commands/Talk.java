31c7ccc04f6d6297c229a30335296c8e123cdd32
ndorazio137 <ndorazio@csumb.edu>
2021-07-19 05:33:14 -0700

    Added talk command

---
+++ src/main/java/LairOfTheForgottenBeast/domain/commands/Talk.java
@@ -0,0 +1,79 @@
+package LairOfTheForgottenBeast.domain.commands;
+
+/* Non-static Imports */
+import java.util.List;
+
+/* In-House Imports */
+import LairOfTheForgottenBeast.domain.GameState;
+import LairOfTheForgottenBeast.domain.Player;
+import LairOfTheForgottenBeast.domain.creature.Creature;
+import LairOfTheForgottenBeast.domain.map.rooms.RoomDynamic;
+import LairOfTheForgottenBeast.domain.prop.Prop;
+
+/**
+ * Represents any command preceded by the word "talk". Implements the ICommand Interface.
+ * 
+ * @author Nick D'Orazio
+ * @version 1.0.0
+ * @since 1.0.0
+ * @see ICommand
+ */
+
+public class Talk implements ICommand<String> {
+  /**
+   * Contains the logic for what to do when the command "talk" is called.
+   * 
+   * @return A String, determined by the Talk logic, and used to update the UI
+   */
+  @Override
+  public <AnyType> String call(GameState gameState, List<String> command) {
+
+    Player player = gameState.getPlayer();
+    RoomDynamic currentRoom = player.getCurrentRoom();
+
+    System.out.println("Current Room to Talk: " + currentRoom.getLongDescription());
+
+    printCommand(command);
+
+    String targetName = buildTargetString(command);
+    Object target = currentRoom.findTarget(targetName);
+    
+    if (target == null)
+      return defaultString();
+    
+    if (target instanceof Prop) {
+      return ((Prop) target).getSpeechText();
+    }
+    
+    if (target instanceof Creature) {
+      return ((Creature) target).getSpeechText();
+    }
+    
+    return defaultString();
+  }
+
+  private String buildTargetString(List<String> command) {
+    String targetName = "";
+    
+    int commandSize = command.size();
+    if (commandSize <= 1) 
+      return defaultString();
+    
+    for (int i = 1; i < command.size(); i++) {
+      if (i > 1) {
+        targetName += " ";
+      }
+      targetName += command.get(i);
+    }
+    
+    return targetName.toUpperCase();
+  }
+  
+  private void printCommand(List<String> command) {
+    System.out.println(command.toString());
+  }
+    
+  private String defaultString() {
+    return "They do not respond.";
+  }
+}
