352d34ed6a2ab5fa3380a48f56bf7e883b9bf6db
Krowi <Krowi@10.0.0.113>
2021-07-18 20:34:14 -0700

    add "public String getItemNameList()" method to Inventory
    and BaseInventory that returns the names of all items in the inventory
    as a List<String>.

--- src/main/java/LairOfTheForgottenBeast/inventorySystem/BaseInventory.java
+++ src/main/java/LairOfTheForgottenBeast/inventorySystem/BaseInventory.java
@@ -9,133 +9,129 @@ import java.util.List;
 import LairOfTheForgottenBeast.domain.prop.Item;
 
 /**
- * Represents a high level inventory container object to be used with the
- * Player, Enemies, Objects, etc.
+ * Represents a high level inventory container object to be used with the Player, Enemies, Objects,
+ * etc.
  * 
  * @author Nick D'Orazio
  * @version 1.0.0
  * @since 1.0.0
  */
-public class BaseInventory implements Inventory, Iterable<Item>
-{
+public class BaseInventory implements Inventory, Iterable<Item> {
 
-   @Override
-   public String toString()
-   {
-      return "" + baseInventory;
-   }
+  @Override
+  public String toString() {
+    return "" + baseInventory;
+  }
 
-   /**
-    * The maximum number of items in a BaseInventory stack.
-    * 
-    * @since 1.0.0
-    */
-   private int MAX_STACK = 50;
-   
-   private int inventoryLimit;
+  /**
+   * The maximum number of items in a BaseInventory stack.
+   * 
+   * @since 1.0.0
+   */
+  private int MAX_STACK = 50;
 
-   /**
-    * The inventory container.
-    * 
-    * @since 1.0.0
-    */
-   private List<Item> baseInventory;
+  private int inventoryLimit;
 
-   /**
-    * Constructor for Inventory Objects. Creates a new inventory of a specific
-    * length when passed a number representing the length of the inventory.
-    * 
-    * @since 1.0.0
-    * @param inventoryLimit The length of the inventory to be created.
-    */
-   public BaseInventory(int inventoryLimit)
-   {
-	  this.inventoryLimit = inventoryLimit;
-      this.baseInventory = new ArrayList<Item>();
-   }
+  /**
+   * The inventory container.
+   * 
+   * @since 1.0.0
+   */
+  private List<Item> baseInventory;
 
-   /**
-    * The size of the maximim number of items in an inventory stack
-    * 
-    * @since 1.0.0
-    */
-   public int getMaxStackSize()
-   {
-      return MAX_STACK;
-   }
+  /**
+   * Constructor for Inventory Objects. Creates a new inventory of a specific length when passed a
+   * number representing the length of the inventory.
+   * 
+   * @since 1.0.0
+   * @param inventoryLimit The length of the inventory to be created.
+   */
+  public BaseInventory(int inventoryLimit) {
+    this.inventoryLimit = inventoryLimit;
+    this.baseInventory = new ArrayList<Item>();
+  }
 
-   /**
-    * Gets the inventory.
-    * 
-    * @since 1.0.0
-    * @return inventory. A List of Items.
-    */
-   public List<Item> getAllItems()
-   {
-      return baseInventory;
-   }
+  /**
+   * The size of the maximim number of items in an inventory stack
+   * 
+   * @since 1.0.0
+   */
+  public int getMaxStackSize() {
+    return MAX_STACK;
+  }
 
-   /* Inherits Javadoc from interface */
-   @Override
-   public int getSize()
-   {
-      return baseInventory.size();
-   }
+  /**
+   * Gets the inventory.
+   * 
+   * @since 1.0.0
+   * @return inventory. A List of Items.
+   */
+  public List<Item> getAllItems() {
+    return baseInventory;
+  }
 
-   /* Inherits Javadoc from interface */
-   @Override
-   public boolean clear()
-   {
-      this.baseInventory = null;
-      baseInventory = new ArrayList<Item>();
+  /* Inherits Javadoc from interface */
+  @Override
+  public int getSize() {
+    return baseInventory.size();
+  }
 
-      /* If inventory is not empty, return failure status false */
-      if (!(baseInventory.isEmpty()))
-         return false;
+  /* Inherits Javadoc from interface */
+  @Override
+  public boolean clear() {
+    this.baseInventory = null;
+    baseInventory = new ArrayList<Item>();
 
-      return true;
-   }
+    /* If inventory is not empty, return failure status false */
+    if (!(baseInventory.isEmpty()))
+      return false;
 
-   /* Inherits Javadoc from interface */
-   @Override
-   public boolean addItem(Item item)
-   {
-      if (item == null)
-         return false;
-      baseInventory.add(item);
-      return true;
-   }
+    return true;
+  }
 
-   /* Inherits Javadoc from interface */
-   @Override
-   public Item removeItem(Item item)
-   {
-      if (baseInventory.isEmpty())
-      {
-         System.out.println("Inventory is Empty");
-         return null;
-      }
-      int index = baseInventory.indexOf(item);
-      Item returnedItem = baseInventory.remove(index);
-      return returnedItem;
-   }
+  /* Inherits Javadoc from interface */
+  @Override
+  public boolean addItem(Item item) {
+    if (item == null)
+      return false;
+    baseInventory.add(item);
+    return true;
+  }
 
-   @Override
-   public Item getItem(String name)
-   {
-      for (Item itemElement : baseInventory)
-      {
-         if (itemElement.getName().equals(name))
-         {
-            return itemElement;
-         }  
-      }
+  /* Inherits Javadoc from interface */
+  @Override
+  public Item removeItem(Item item) {
+    if (baseInventory.isEmpty()) {
+      System.out.println("Inventory is Empty");
       return null;
-   }
+    }
+    int index = baseInventory.indexOf(item);
+    Item returnedItem = baseInventory.remove(index);
+    return returnedItem;
+  }
 
-	@Override
-	public Iterator<Item> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Item getItem(String name) {
+    for (Item itemElement : baseInventory) {
+      if (itemElement.getName().equals(name)) {
+        return itemElement;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public Iterator<Item> iterator() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<String> getItemNameList() {
+    List<String> itemNameList = new ArrayList<String>();
+    for (Item item : baseInventory) {
+      itemNameList.add(item.getName());
+    }
+    return itemNameList;
+  }
 }
